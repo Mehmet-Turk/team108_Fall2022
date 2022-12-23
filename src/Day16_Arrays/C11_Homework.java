9c9fa78b25b0fcd0cdf3be0b8231509935819b27
Mehmet-Turk <turk.mehmet0627@gmail.com>
2022-12-23 22:19:05 +0100

    day17

--- src/Day16_Arrays/C11_Homework.java
+++ src/Day16_Arrays/C11_Homework.java
@@ -1,9 +1,31 @@
 package Day16_Arrays;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class C11_Homework {
     public static void main(String[] args) {
         //kullanicidan isimler alip varolan bir arraye C10 daki methodu kullanarak kullanici eklemeye
         //devam ettikce devam edim
         //kullanici Q ya basinca Arrayin son halini dondurun
+        String[] isimler = {"Ali", "Mustafa", "Can", "Ayse"};
+        isimler = isimAlma(isimler);
+        System.out.println(Arrays.toString(isimler));
+    }
+    public static String[] isimAlma(String[] array){
+        Scanner scan = new Scanner(System.in);
+        String eklenecekIsim = null;
+        /*while (!eklenecekIsim.equals("Q")){
+            System.out.println("Lutfen bir isim giriniz: ");
+            eklenecekIsim = scan.next();
+            array = C10_NewVariableArray.arrayeElemanEkleme(array, eklenecekIsim);
+        }*/
+        do {
+            System.out.println("Lutfen bir isim giriniz: ");
+            eklenecekIsim = scan.next();
+            if (eklenecekIsim.equals("Q"))break;
+            array = C10_NewVariableArray.arrayeElemanEkleme(array, eklenecekIsim);
+        }while (!eklenecekIsim.equals("Q"));
+        return array;
     }
 }
--- src/Day16_Arrays/C13_BinarySearch.java
+++ src/Day16_Arrays/C13_BinarySearch.java
@@ -12,8 +12,10 @@ public class C13_BinarySearch {
 
 
         Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr));//[1, 2, 3, 4, 6, 89, 90]
         System.out.println(Arrays.binarySearch(arr, 3));//aranan elemanin indexini doner
-        System.out.println(Arrays.binarySearch(arr, 0));//eger bulamazsa -1 doner
+        System.out.println(Arrays.binarySearch(arr, 0));//eger bulamazsa eger olsaydi kacinci sirada olacaksa
+        //onun eksi halini doner -4 -6 gibi doner
+        System.out.println(Arrays.binarySearch(arr, 5));//-
     }
 }
